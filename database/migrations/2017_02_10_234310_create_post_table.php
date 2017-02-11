8b81640b29344e0667efc9e7ba05f7506a050e4c
vS0uz4 <vitor.rodrigues@gmail.com>
2017-02-11 10:20:02 -0200

    Create migrations for Posts, Comments, Tags and PivotTable

---
+++ database/migrations/2017_02_10_234310_create_post_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 255);
+            $table->text('content');
+
+            $table->integer('author_id')->unsigned();
+            $table->foreign('author_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
