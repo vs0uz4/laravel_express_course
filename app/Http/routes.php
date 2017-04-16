d91b201064c9781b2b392a36285275c4c56c1bf5
vS0uz4 <vitor.rodrigues@gmail.com>
2017-04-16 10:58:07 -0300

    Refactoring routes
    Adjusting the control panel routes for posts, comments and dashboard, and for the home pages, about and contact

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,12 +14,30 @@
 /*
  * Routes for pages of blog
  */
-Route::get('/',         'Pages\HomeController@index');
-Route::get('/about',    'Pages\AboutController@index');
-Route::get('/contact',  'Pages\ContactController@index');
+Route::get('/',         ['as'=>'blog.index',    'uses' => 'Pages\HomeController@index']);
+Route::get('/about',    ['as'=>'blog.about',    'uses' => 'Pages\AboutController@index']);
+Route::get('/contact',  ['as'=>'blog.contact',  'uses' => 'Pages\ContactController@index']);
 
 /*
  * Routes for administrative panel of blog
  */
-Route::get('/panel',        'Panel\DashboardController@index');
-Route::get('/panel/posts',  'Panel\PostsController@index');
+Route::group(['prefix' => 'panel'], function(){
+    Route::get('',                      ['as' => 'panel.dashboard.index',   'uses' => 'Panel\DashboardController@index']);
+
+    Route::group(['prefix' => 'posts'], function(){
+        Route::get('',                  ['as' => 'panel.posts.index',       'uses' => 'Panel\PostsController@index']);
+        Route::get('create',            ['as' => 'panel.posts.create',      'uses' => 'Panel\PostsController@create']);
+        Route::post('store',            ['as' => 'panel.posts.store',       'uses' => 'Panel\PostsController@store']);
+        Route::get('{id}/edit',         ['as' => 'panel.posts.edit',        'uses' => 'Panel\PostsController@edit']);
+        Route::put('{id}/update',       ['as' => 'panel.posts.update',      'uses' => 'Panel\PostsController@update']);
+        Route::delete('{id}/destroy',   ['as' => 'panel.posts.destroy',     'uses' => 'Panel\PostsController@destroy']);
+        Route::get('{id}/comments',     ['as' => 'panel.posts.comments',    'uses' => 'Panel\PostsController@comments']);
+    });
+
+    Route::group(['prefix' => 'comments'], function(){
+        Route::get('',                      ['as' => 'panel.comments.index',            'uses' => 'Panel\CommentsController@index']);
+        Route::put('{id}/toggleConfirm',    ['as' => 'panel.comments.toggleConfirm',    'uses' => 'Panel\CommentsController@toggleConfirm']);
+        Route::delete('destroy/{id}',       ['as' => 'panel.comments.destroy',          'uses' => 'Panel\CommentsController@destroy']);
+    });
+
+});
